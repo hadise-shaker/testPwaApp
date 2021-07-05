2e03aec5598c73a5db4561839d99c448b57e5abe
hadise-shaker <hadise_shaker66@yahoo.com>
2021-07-05 20:31:49 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      MY APP :)))))))
+    </div>
+  );
+}
+
+export default App;
